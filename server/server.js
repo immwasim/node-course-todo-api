0576e3caebebf1836b0bc8cde8519fef866d1df5
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-23 23:55:41 -0400

    add get todo by id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -38,9 +39,29 @@ app.get('/todos', (req, res) => {
     );
 });
 
+app.get('/todos/:id', (req,res) => {
+    var id=req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    Todo.findById(id)
+    .then((todo) => {
+        if(!todo){
+            res.status(400).send();
+        }
+        res.status(200)
+        .send({todo});
+    })
+    .catch((e) => {res.status(400).send();});
+
+});
+
 app.listen(3000, () => {
     console.log('server started on 3000');
 });
+
+
 
 module.exports = {app};
 
