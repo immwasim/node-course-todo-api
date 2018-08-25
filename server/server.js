2e5baf2bb16d92ffdd12209b9770756bdf9cc094
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-25 13:22:00 -0400

    set up env configuration

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 const _ = require('lodash');
 
 var express = require('express');
@@ -10,7 +11,7 @@ var {User} = require('./models/user');
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
