8538ea8174873dd2df71852ec573ba9b2bda6dc5
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-24 23:13:14 -0400

    added patch todos by id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -74,6 +76,34 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400)
         .send();
     });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']); 
+    //only take necessary props from input
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send({e:'invalid id'});
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body}, {new:true})
+    .then((todo) => {
+        if(!todo){
+            res.status(404).send({e:'coudl not update'});
+        }
+        res.send({todo});
+    })
+    .catch((e) => {
+        res.status(400).send();
+    })
 });
 
 app.listen(port, () => {
