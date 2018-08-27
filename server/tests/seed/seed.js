255bca44eb56ca82ebcc8c94c8fdb371c212008e
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-27 03:15:28 -0400

    added tests for get /users/me and post /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,63 @@
+const { ObjectID } = require('mongodb');
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+const jwt = require('jsonwebtoken');
+
+const uoneid = new ObjectID();
+const utwoid = new ObjectID();
+
+const users = [
+    {
+        _id: uoneid,
+        email: 'qqddda@was.com',
+        password: 'User1Pass',
+        tokens: [
+            {
+                access: 'auth',
+                token: jwt.sign({ _id: uoneid, access: 'auth' }, '123123').toString()
+            }
+        ]
+    },
+    {
+        _id: utwoid,
+        email: 'rrsddddr@was.com',
+        password: 'User2Pass'
+    }
+];
+
+const seedData = [
+    {
+        _id: new ObjectID(),
+        text: 'First to do'
+    },
+    {
+        _id: new ObjectID(),
+        text: 'second to do',
+        completed: true,
+        completedAt: 333
+    }
+]
+
+const populateTodos = (done) => {
+    Todo.deleteMany({})
+        .then(() => {
+            return Todo.insertMany(seedData);
+        })
+        .then(() => done());
+};
+
+const populateUsers = (done) => {
+    User.deleteMany({})
+        .then(() => {
+            // cant use insertMany because 
+            //we need the save function to trigger the pre save hook
+            // that hashes and slats the password
+            // This is async therefore we must wait till the process is completed 
+            var userOne = new User(users[0]).save();
+            var userTwo = new User(users[1]).save();
+            return Promise.all([userOne, userTwo])
+        })
+        .then(() => done());
+};
+
+module.exports = { seedData, populateTodos, users, populateUsers };
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,29 +3,14 @@ const request = require("supertest");
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
 
 const {ObjectID} = require('mongodb');
+const {seedData, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const seedData = [
-    {
-        _id: new ObjectID(),
-        text:'First to do'
-    },
-    {
-        _id: new ObjectID(),
-        text:'second to do',
-        completed:true,
-        completedAt:333
-    } 
-]
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
-beforeEach((done) => {
-    Todo.deleteMany({})
-    .then(()=>{
-        return Todo.insertMany(seedData);
-    })
-    .then(() => done());
-});
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -185,3 +170,82 @@ describe('PATCH todos/:id', () => {
         .end(done);
     });
 });
+
+
+describe('GET users/me', ()=>{
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);
+            })
+            .end(done);
+    })
+
+    it('should return 401 if not authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect((res) => {
+                expect(res.body).toEqual({});
+            })
+            .end(done)
+    })
+});
+
+describe('POST /users/', ()=>{
+    it('should create a user', (done) => {
+        var email = 'swsws@was.com';
+        var password = 'ertertert';
+
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(200)
+        .expect((res) => {
+            expect(res.headers['x-auth']).toExist();
+            expect(res.body._id).toExist();
+            expect(res.body.email).toBe(email);
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+            User.findOne({email})
+            .then((user) => {
+                expect(user).toExist();
+                expect(user.password).toNotBe(password);
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        })
+    })
+
+    it('should return validation errors if request is invalid', (done) => {
+        var email = 'swswswwas.com';
+        var password = 'rtert';
+
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done)
+
+    })
+
+    it('should not create a user if email in use', (done) => {
+        var email = users[0].email;
+        var password = 'rtersdt';
+        
+        request(app)
+        .post('/users')
+        .send({email, password})
+        .expect(400)
+        .end(done)
+    })
+}
+);
