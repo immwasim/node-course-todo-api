c1ba34f37d39888b92801ade7b12c21e5ad62aca
immwasim <wasim.singh1@sheridancollege.ca>
2018-08-27 00:45:25 -0400

    hash user password

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
-const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
 
 
@@ -87,6 +87,21 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     })
 }
+
+UserSchema.pre('save', function(next){
+    var user = this
+
+    if(user.isModified('password')){
+        bcrypt.genSalt(10, (err, salt)=>{
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                user.password = hash;
+                next();
+            });
+        })
+    }else{
+        next();
+    }
+})
 
 var User = mongoose.model('User', UserSchema);
 
